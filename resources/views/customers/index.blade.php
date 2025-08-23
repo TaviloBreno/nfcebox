6325d67264e24679cb16c08aa1b60c04926ea2a2
Tavilo Breno <breno_wk2@hotmail.com>
2025-08-23 15:10:14 -0300

    Fix customer page display issue - correct PHP syntax in data-table array

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -72,25 +72,7 @@
                                 'email' => $customer->email,
                                 'telefone' => $customer->phone ? '<span class="font-monospace">' . $customer->formatted_phone . '</span>' : '<span class="text-muted">-</span>',
                                 'cidade' => $customer->address ? ($customer->address['city'] ?? '') . ', ' . ($customer->address['state'] ?? '') : '-',
-                                'acoes' => '
-                                    <div class="btn-group" role="group">
-                                        <a href="' . route('customers.show', $customer) . '" 
-                                           class="btn btn-sm btn-outline-info" 
-                                           title="Visualizar">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                        <a href="' . route('customers.edit', $customer) . '" 
-                                           class="btn btn-sm btn-outline-primary" 
-                                           title="Editar">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                        <button type="button" 
-                                                class="btn btn-sm btn-outline-danger" 
-                                                title="Excluir"
-                                                onclick="confirmDelete(' . $customer->id . ', \'' . $customer->name . '\')">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </div>'
+                                'acoes' => '<div class="btn-group" role="group"><a href="' . route('customers.show', $customer) . '" class="btn btn-sm btn-outline-info" title="Visualizar"><i class="fas fa-eye"></i></a><a href="' . route('customers.edit', $customer) . '" class="btn btn-sm btn-outline-primary" title="Editar"><i class="fas fa-edit"></i></a><button type="button" class="btn btn-sm btn-outline-danger" title="Excluir" onclick="confirmDelete(' . $customer->id . ', \'' . addslashes($customer->name) . '\');"><i class="fas fa-trash"></i></button></div>'
                             ];
                         })"
                         striped
